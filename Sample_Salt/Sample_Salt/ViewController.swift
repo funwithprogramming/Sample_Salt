bf9175767c20101b2fa1cb59467164a07b6abebe
Alekh <alekh.mittal@aspiringminds.in>
2016-02-15 13:17:15 +0530

    Project added

---
+++ Sample_Salt/Sample_Salt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sample_Salt
+//
+//  Created by Alekh on 15/02/16.
+//  Copyright © 2016 Alekh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
